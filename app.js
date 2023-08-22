3e35ad12c7b2a231b69192e48dce6f6fff01ab65
LastGregStanding <125837709+LastGregStanding@users.noreply.github.com>
2023-08-21 23:39:21 -0700

    added hovering and changed a few things

--- app.js
+++ app.js
@@ -5,3 +5,28 @@
 add, substract, multiply, divide
 
 */
+
+const smallBtn = document.querySelectorAll(".small-button");
+const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+
+smallBtn.forEach((btn) => {
+  btn.addEventListener("mouseover", () => btn.classList.add("hover-small-btn"));
+  btn.addEventListener("mouseout", () =>
+    btn.classList.remove("hover-small-btn")
+  );
+});
+
+clearBtn.addEventListener("mouseover", function () {
+  clearBtn.classList.add("hover-clear-btn");
+});
+clearBtn.addEventListener("mouseout", function () {
+  clearBtn.classList.remove("hover-clear-btn");
+});
+
+deleteBtn.addEventListener("mouseover", function () {
+  deleteBtn.classList.add("hover-delete-btn");
+});
+deleteBtn.addEventListener("mouseout", function () {
+  deleteBtn.classList.remove("hover-delete-btn");
+});
